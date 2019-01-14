fbe794a5a60baedbf675f14dbaa5f1a2baa33ca2
duli <duli@iwisedev.com>
2019-01-14 16:35:58 +0800

    hash

--- geekpractice/src/bsearch/Bsearch_3.java
+++ geekpractice/src/bsearch/Bsearch_3.java
@@ -1,10 +1,24 @@
 package bsearch;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 //查找最后一个小于等于某个值
 public class Bsearch_3 {
 
     public static void main(String[] args) {
-        int[] a = {0,1,2,2,2,2,3,4,5,6,7,8,9,10};
-        System.out.println(bsearch(a,2));
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2018,12-1,31);
+        long a = calendar.getTimeInMillis();
+        Date date = new Date(a);
+        System.out.println(date);
+        String d1 = new SimpleDateFormat("yyyy-MM-dd").format(date);
+        String d2 = new SimpleDateFormat("YYYY-MM-dd").format(date);
+        System.out.println(d1);
+        System.out.println(d2);
+//        int[] a = {0,1,2,2,2,2,3,4,5,6,7,8,9,10};
+//        System.out.println(bsearch(a,2));
     }
 
     public static int bsearch(int[] a,int key){
