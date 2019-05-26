44a41d97ec215c77a2c469c5a6e9cb51c380f04b
592306832@qq.com <592306832@qq.com>
2019-05-26 20:07:17 +0800

    observer

---
+++ design/src/observer/WeatherStation.java
@@ -0,0 +1,18 @@
+package observer;
+
+public class WeatherStation {
+
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+
+        weatherData.setMeasurements(80,65,34.2f);
+
+    }
+
+
+
+
+
+}
